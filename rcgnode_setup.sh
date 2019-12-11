36652b1c5e4552eeb8893817551c527d24a4b931
ThisIsNotAValidUserName <zhudi217@foxmail.com>
2019-12-10 21:58:54 -0500

    Create rcgnode_setup.sh

---
+++ rcgnode_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "updating apt-get"
+apt-get update
+echo "installing dependencies"
+apt-get install python3-pip
+apt-get install wget
+python3 -m pip install --upgrade google-cloud-vision
+echo "downloading python codes"
+wget https://raw.githubusercontent.com/zhangchi1/ImageRecognitionWeb/master/rcgNode/recognition.py
+wget https://raw.githubusercontent.com/zhangchi1/ImageRecognitionWeb/master/rcgNode/server.py
+wget https://raw.githubusercontent.com/ThisIsNotAValidUserName/Geni-mini-project/master/CS655FinalKey.json
